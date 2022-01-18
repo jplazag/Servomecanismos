de6abf3a34cc2470f7e7224ddc892d2ee034f598
Ana Maria R <anmrdz@gmail.com>
2022-01-17 21:55:44 -0500

    Add motor model

--- Modelo Simscape completo/ModeloMotor.m
+++ Modelo Simscape completo/ModeloMotor.m
@@ -1,10 +1,10 @@
-% Modelo motor%
-
-Va = 9.55; % V
-Ra = 0.79; % Ohms
-La = 0.37*10^-3; % mH
-Kt = 0.009; % Constante de torque (N m)/A
-b = ;
-J = ;
-Kb = ;
-TL
+% Modelo motor DC054B–2%
+% VA = 24V
+Va = 24; % V
+Ra = 1.73; % Ohms
+La = 2.5; % mH
+Kt = 0.0551; % Constante de torque (N m)/A
+b = 1.1E-05; % (N m) / (rad/s)
+J = 1.6E-05; % kg*m^2
+Kb = 0.0551; % V/(rad/s)
+TL = 0;

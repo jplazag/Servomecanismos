5a42ab202e8a17862f0f3b1d3d83250c25b278ed
YeisonGonzalez <43708292+YeisonGonzalez@users.noreply.github.com>
2022-01-22 16:34:03 -0500

    Intento de utilizar el plugin fallido

---
+++ Modelo prueba con correa/Prueba.m
@@ -0,0 +1,7 @@
+% Copyright 2018 The MathWorks, Inc.
+addpath(pwd);
+addpath([pwd filesep 'CAD']);
+addpath([pwd filesep 'Images']);
+addpath([pwd filesep 'Scripts_Data']);
+addpath([pwd filesep 'html' filesep 'html']);
+sm_tower_crane_hoist

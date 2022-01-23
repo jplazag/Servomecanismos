cfa254e637574782741ee6380a1188dcdd6f852e
Ana Maria R <anmrdz@gmail.com>
2022-01-23 16:09:55 -0500

    Add identification of plants

--- Modelo Simscape completo con motor/ModeloControl.m
+++ Modelo Simscape completo con motor/ModeloControl.m
@@ -6,26 +6,54 @@ P2 = 1;
 
 %% MODELO MOTORES
 
-% Modelo motor DC054B–2%
+% Modelo motor 1 - REF: DC054B–2
 % VA = 24V
-Va = 24; % V
-Ra = 1.73; % Ohms
-La = 2.5; % mH
-Kt = 0.0551; % Constante de torque (N m)/A
-b = 1.1E-05; % (N m) / (rad/s)
-J = 1.6E-05; % kg*m^2
-Kb = 0.0551; % V/(rad/s)
-TL = 0;
+Va1 = 24; % V
+Ra1 = 1.73; % Ohms
+La1 = 2.5; % mH
+Kt1 = 0.0551; % Constante de torque (N m)/A
+b1 = 1.1E-05; % (N m) / (rad/s)
+J1 = 1.6E-05; % kg*m^2
+Kb1 = 0.0551; % V/(rad/s)
+TL1 = 0;
 
+% Modelo motor 1 - REF: DC054B–2
+% VA = 24V
+Va2 = Va1; % V
+Ra2 = Ra1; % Ohms
+La2 = La1; % mH
+Kt2 = Kt1; % Constante de torque (N m)/A
+b2 = b1; % (N m) / (rad/s)
+J2 = J1; % kg*m^2
+Kb2 = kb1; % V/(rad/s)
+TL2 = 0;
 
-%% Caracterización
-A = [2.03034366646370,338.129178581801];
-B = [1.20000573358804,170.780488526573];
+%% Caracterización 
 
-x = linspace(0,10);
-m = (B(2)-B(1))/(A(2)-A(1));
-n = B(2)*m - A(2);
-y = m*x + n;
+% data1 = load('CaracterizacionPlanta1.mat').data ;
+planta1 = extractTimetable(load('CaracterizacionPlanta1.mat').data );
+planta2 = extractTimetable(load('CaracterizacionPlanta2.mat').data );
 
-line(x,y)
-hold on
+[p1, tau_m1, km1] = identificarPlanta(seconds(planta1.Time), planta1.theta1, 20);
+[p2, tau_m2, km2] = identificarPlanta(seconds(planta2.Time), planta2.theta2, 20);
+
+%% Functions
+function [p, tau_m, Km] = identificarPlanta(x, y, A)
+    figure
+
+    % zona de rampa para tiempo de caract 2.5
+    init = 0.12; % 0.3 s inicio
+    endr = 0.80; % 2 s fin
+    samples = size(x, 1);
+    x_ramp = x(round(init*samples):round(endr*samples));
+    y_ramp = y(round(init*samples):round(endr*samples));
+    p = polyfit(x_ramp,y_ramp,1); 
+    Km = p(1)/A;
+    tau_m = roots(p);
+    f = polyval(p,x_ramp); 
+    plot(x,y,x_ramp,f,'--') 
+    title('Respuesta al paso')
+    ylabel('Posición (rad)')
+    xlabel('Tiempo (s)')
+    grid on
+end
